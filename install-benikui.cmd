1e9131b3c54b0e6fe0abc8c00b99cd985ac5b5f5
PotatoIRL <howishoopwoop@gmail.com>
2018-05-07 21:55:39 -0400

    add new benikui script

---
+++ install-benikui.cmd
@@ -0,0 +1,27 @@
+@setlocal enabledelayedexpansion
+@echo off
+
+set ADDONS_FOLDER=F:\World of Warcraft\Interface\AddOns
+set ELVUI_ARCHIVE=https://git.tukui.org/elvui/elvui/repository/master/archive.zip
+set BENIKUI_ARCHIVE=https://www.tukui.org/addons.php?download=11
+
+set ZIP_FILE=elvui.zip
+call curl %ELVUI_ARCHIVE% > %ZIP_FILE%
+call 7z x -y -o"%ADDONS_FOLDER%" %ZIP_FILE% | findstr /B /V /I "Extracting"
+for /f "tokens=1" %%g in ('ls "%ADDONS_FOLDER%" ^| findstr elvui-master*') do ( 
+	for /d %%a in ("%ADDONS_FOLDER%\%%g\*.*") do (
+		rmdir /S /Q "%ADDONS_FOLDER%\%%~nxa"
+		move /Y "%%a" "%ADDONS_FOLDER%"
+	)
+	echo rmdir /S /Q %ADDONS_FOLDER%\%%g
+)
+call del /S /Q %ZIP_FILE%
+
+set ZIP_FILE=benikui.zip
+call curl %BENIKUI_ARCHIVE% > %ZIP_FILE%
+call 7z x -y -o"%ADDONS_FOLDER%" %ZIP_FILE% | findstr /B /V /I "Extracting"
+call del /S /Q %ZIP_FILE%
+
+:end
+@endlocal
+exit /b 0
